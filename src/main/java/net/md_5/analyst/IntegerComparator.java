06741212b20224e76d2e78a803b57c333aac3d98
md_5 <md_5@bigpond.com>
2012-12-05 17:34:42 +1100

    Small cleanup

--- src/main/java/net/md_5/analyst/IntegerComparator.java
+++ src/main/java/net/md_5/analyst/IntegerComparator.java
@@ -4,21 +4,21 @@ import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class IntegerComparator implements Comparator<Object> {
+public class IntegerComparator<K> implements Comparator<K> {
 
-    private final Map<?, ? extends Number> toSort;
+    private final Map<K, ? extends Number> toSort;
 
-    public IntegerComparator(Map<?, ? extends Number> toSort) {
+    public IntegerComparator(Map<K, ? extends Number> toSort) {
         this.toSort = toSort;
     }
 
     @Override
-    public int compare(Object key1, Object key2) {
-        return (toSort.get(key1).longValue() < toSort.get(key2).longValue()) ? 1 : -1;
+    public int compare(K key1, K key2) {
+        return toSort.get(key1).longValue() < toSort.get(key2).longValue() ? 1 : -1;
     }
 
     public static <K, V extends Number> Map<K, V> sort(Map<K, V> input) {
-        TreeMap<K, V> tree = new TreeMap<>(new IntegerComparator(input));
+        TreeMap<K, V> tree = new TreeMap<>(new IntegerComparator<>(input));
         tree.putAll(input);
         return tree;
     }
